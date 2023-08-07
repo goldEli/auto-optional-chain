02fcf1c2de2cb7345bddbaf95769cd01f2c06089
缪宇 <miaoyu2009@qq.com>
2023-08-07 09:53:08 +0800

    feat: use eslint api

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const { ESLint } = require("eslint");
+
+const engine = new ESLint({
+  fix: true,
+  overrideConfig: {
+    parser: "@babel/eslint-parser",
+    rules: {
+      semi: ["error", "never"],
+    },
+  },
+  useEslintrc: false,
+});
+
+async function main() {
+  const results = await engine.lintText(`
+        function handleRes(data) {
+            const res = data.a.b.c + data.e.f.g;
+
+        }
+    `);
+  console.log(results[0].output);
+  /**
+   * 美化输出结果
+   */
+  const formatter = await engine.loadFormatter("stylish");
+  const resultText = formatter.format(results);
+  console.log(resultText);
+}
+main();
